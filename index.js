2c77e8ee9c70d38aa272ca136c7de18dfd64b502
unknown <pradnyadolthade236@gmail.com>
2024-02-26 22:21:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const containerEl = document.querySelector('.projects__container')
+var mixer = mixitup(containerEl,{
+    animation :{
+        enable:false
+    }
+});
+
+mixer.filter('*');
+
+
+
+const navMenu = document.querySelector('.nav__menu')
+const navOpenBtn = document.querySelector('.nav__toggle-open')
+const navCloseBtn = document.querySelector('.nav__toggle-close')
+
+const openNavHandler=()=>{
+    navMenu.style.display= 'flex';
+    navOpenBtn.style.display='none';
+    navCloseBtn.style.display='inline-block';
+
+
+}
+const closeNavHandler=()=>{
+    navMenu.style.display= 'none';
+    navOpenBtn.style.display='inline-block';
+    navCloseBtn.style.display='none';
+
+
+}
+
+
+navOpenBtn.addEventListener('click',openNavHandler)
+navCloseBtn.addEventListener('click',closeNavHandler)
+
+
+const navItems = navMenu.querySelectorAll('a');
+if(window.innerWidth<768){
+    navItems.forEach(items =>{
+        items.addEventListener('click', closeNavHandler)
+    })
+}
+
+
+
+
+const themeBtn = document.querySelector('.nav__theme-btn');
+themeBtn.addEventListener('click', () => {
+    let bodyClass = document.body.className;
+    if(!bodyClass){
+        bodyClass = 'dark';
+        document.body.className = bodyClass;
+        themeBtn.innerHTML = "<i class='uil uil-sun'></i>"
+        //save theme to local storage
+        window.localStorage.setItem('theme', bodyClass);
+    }
+    else{
+        bodyClass = '';
+        document.body.className = bodyClass;
+        themeBtn.innerHTML = "<i class='uil uil-moon'></i>"
+         //save theme to local storage
+         window.localStorage.setItem('theme', bodyClass);
+
+
+    }
+})
+
+
+
+//load theme on load
+window.addEventListener('load',()=>{
+    document.body.className= window.localStorage.getItem('theme');
+})
